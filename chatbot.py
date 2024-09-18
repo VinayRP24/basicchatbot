f2a9eb5a9c4893808bb7e18782ea5b3b0b62508e
VinayRP24 <vinayrparab1@gmail.com>
2024-09-18 14:59:29 -0400

    checks documents

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,11 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
 
 # get keys
 env = load_dotenv(find_dotenv())
@@ -11,15 +14,51 @@ openai = os.environ["OPENAI_API_KEY"]
 # connect to ChatGPT
 chatbot = ChatOpenAI(model="gpt-3.5-turbo")
 
+# create documents
+documents = [
+    Document(
+        page_content="Vinay is very cool. He is know for being very smart and diligent. He once fought a bear!",
+        metadata={"source": "vinay-doc"},
+    ),
+    Document(
+        page_content="Noah is very bored",
+        metadata={"source": "noah-doc"},
+    ),
+    Document(
+        page_content="Riya is very happy",
+        metadata={"source": "riya-doc"},
+    ),
+    Document(
+        page_content="Eve is very sad",
+        metadata={"source": "eve-doc"},
+    ),
+]
+
+vector_store = Chroma.from_documents(documents, embedding=OpenAIEmbeddings())
+
+# retriever
+retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k":1}) # best result
+
+# set up 
+l = input("Choose language: ")
+question = """
+Answer this question using the provided context.
+Additionaly translate the answer from English into """ + l + """
+Tell me more about {name}
+
+Context: {context}
+"""
+prompt = ChatPromptTemplate.from_messages([("system", question)])
+
 # set up message
-temp = "Translate the following from English into French (Canada)"
-m = input("Enter message: ")
-messages = [SystemMessage(content=temp), HumanMessage(content=m)]
+
+m = input("Enter name: ")
 
 # set up parser
 parser = StrOutputParser()
 
 # chain prompt
-chain = chatbot | parser
-response = chain.invoke(messages)
+chain = {"context": retriever, 
+    "name": RunnablePassthrough()} | prompt | chatbot | parser
+response = chain.invoke(m)
 print(response)
